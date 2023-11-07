bac309152e396b69090c315904def744027ab797
Wixden <watchonlytutorial@gmail.com>
2023-11-07 06:46:50 +0600

    Chapter 1 complete

--- src/class-and-object.ts
+++ src/class-and-object.ts
@@ -10,10 +10,34 @@
     constructor(name: string, species: string, sound: string) {
       (this.name = name), (this.species = species), (this.sound = sound);
     }
+
+    // function
+    makeSound() {
+      console.log(`The ${this.species} sound is ${this.sound}`);
+    }
   }
 
-  const dog = new Animal("Sparky", "Dog", "woof");
-  const cat = new Animal("Tommy", "Cat", "Meow Meow");
+  const animal1 = new Animal("Sparky", "Dog", "woof");
+  const animal2 = new Animal("Tommy", "Cat", "Meow Meow");
+
+  animal1.makeSound();
+  animal2.makeSound();
+
+  // parameter properties (here we don't need to define the property types, we can declare them directly in constructor with public keyword)
+  class Person {
+    constructor(
+      public name: string,
+      public age: number,
+      public gender: string
+    ) {}
+
+    isMaleOrFemale() {
+      console.log(`${this.name} is ${this.gender}`);
+    }
+  }
+
+  const person = new Person("John", 30, "Gay");
+  person.isMaleOrFemale();
 
   // ==============================================================
 }
