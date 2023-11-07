86c7c9b0486943962b1d003fd370b1d11b3918a0
Wixden <watchonlytutorial@gmail.com>
2023-11-08 03:51:17 +0600

    in guard works only in object

--- src/typeGuard-typeof-and-typein.ts
+++ src/typeGuard-typeof-and-typein.ts
@@ -19,5 +19,27 @@
   console.log(result1);
   console.log(5);
 
+  // in guard
+  type NormalUser = {
+    name: string;
+  };
+  type AdminUser = {
+    name: string;
+    role: string;
+  };
+
+  const getUser = (user: NormalUser | AdminUser) => {
+    // You cannot access the role property in here.
+    // user. // will suggest only name. because name property is common in both types of users.
+    if ("role" in user) {
+      // user.role // Now we will get suggestion for both the name and role. because we are using in guard to
+      console.log(`My name is ${user.name} and my role is ${user.role}`);
+    } else {
+      console.log(`My name is ${user.name}`);
+    }
+  };
+
+  // You can access the role property in here.
+
   // ==============================================================
 }
