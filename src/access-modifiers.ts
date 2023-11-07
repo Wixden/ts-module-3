8f084e793f5cc5ae55f646317154ea838bf8373b
Wixden <watchonlytutorial@gmail.com>
2023-11-08 05:36:01 +0600

    Public and Private access modifier. private is only accessible withing its scope and public can be accessed outside

--- src/access-modifiers.ts
+++ src/access-modifiers.ts
@@ -5,9 +5,9 @@
 
   // to restrict access or to restrict modification we will use readonly before property name.
   class BankAccount {
-    readonly id: number;
-    name: string;
-    readonly balance: number;
+    public readonly id: number;
+    public name: string;
+    private balance: number;
 
     constructor(id: number, name: string, balance: number) {
       this.id = id;
@@ -23,6 +23,8 @@
   const personOne = new BankAccount(134567, "John Smith", 56000);
   personOne.name = "Mark Benjamin"; // we can change the name because it is not readonly
   // personOne.id = 134567; // now here we can't modify the value of id, which is readonly.
+
+  // personOne.balance = 2; // we can't change the value of balance because it is private, which can only be accessed inside the class scope.
 
   // ==============================================================
 }
