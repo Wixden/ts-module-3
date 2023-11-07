c2345f7c7ba24c6bab3b36299fc4adf38d336ebe
Wixden <watchonlytutorial@gmail.com>
2023-11-07 16:29:43 +0600

    Chapter 2 Complete

--- src/inheritance-in-oop.ts
+++ src/inheritance-in-oop.ts
@@ -45,6 +45,37 @@
     "Chicago",
     55
   );
+  console.log(student1);
+  student1.sleepHour(10);
+  student1.studyHour(10);
+
+  class Teacher extends User {
+    designation: string;
+    constructor(
+      name: string,
+      email: string,
+      gender: string,
+      address: string,
+      designation: string
+    ) {
+      super(name, email, gender, address);
+      this.designation = designation;
+    }
+
+    teachHour(hours: number) {
+      console.log(`${this.name} will teach for ${hours}`);
+    }
+  }
+
+  const teacher1 = new Teacher(
+    "Michel Markov",
+    "michelmarkov@gmail.com",
+    "Male",
+    "New York",
+    "Professor"
+  );
+  console.log(teacher1);
+  teacher1.teachHour(10);
 
   // ==============================================================
 }
