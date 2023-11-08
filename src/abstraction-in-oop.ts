fa859c187fbba17679196a710d4659e8903818dc
Wixden <watchonlytutorial@gmail.com>
2023-11-09 03:55:33 +0600

    Chapter 9 complete

--- src/abstraction-in-oop.ts
+++ src/abstraction-in-oop.ts
@@ -27,5 +27,30 @@
   toyota.stopEngine();
   toyota.move();
 
+  // idea : abstract - interface - abstract
+  // we cannot create instance of Abstract class. Instead we create a new class and extends it to inherit the properties
+  abstract class Car2 {
+    abstract startEngine(): void;
+    abstract stopEngine(): void;
+    abstract move(): void;
+  }
+
+  class ToyotaCar extends Car2 {
+    startEngine(): void {
+      console.log("Toyota Car startEngine is starting");
+    }
+    stopEngine(): void {
+      console.log("Toyota Car stopEngine is stopping");
+    }
+    move(): void {
+      console.log("Toyota Car is moving");
+    }
+  }
+
+  const toyota2 = new ToyotaCar();
+  toyota2.startEngine();
+  toyota2.stopEngine();
+  toyota2.move();
+
   // ===============================================================================
 }
