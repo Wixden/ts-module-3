6357d3ac48f70c34902019138ab2b9e1d8e47fde
Wixden <watchonlytutorial@gmail.com>
2023-11-08 06:52:45 +0600

    using the function as a property of class.

---
+++ src/getter-and-setter.ts
@@ -0,0 +1,47 @@
+{
+  // ==============================================================
+
+  // getter and setter
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    private _balance: number;
+    protected _type: string;
+
+    constructor(id: number, name: string, balance: number, type: string) {
+      this.id = id;
+      this.name = name;
+      this._balance = balance;
+      this._type = type;
+    }
+
+    addBalance(amount: number) {
+      const newBalance = (this._balance += amount);
+      return `$${amount} added and new balance $${newBalance}`;
+    }
+
+    // doing above function using getter and setter
+    // getter
+    get balance() {
+      return this._balance;
+    }
+
+    getBalance() {
+      return this._balance;
+    }
+  }
+
+  const personOne = new BankAccount(134567, "John Smith", 56000, "Savings");
+  personOne.name = "Mark Benjamin";
+
+  const addMoney = personOne.addBalance(5000);
+  console.log(addMoney);
+
+  const myBalance = personOne.getBalance();
+  // console.log(`My balance: $${myBalance}`);
+
+  const myBalance2 = personOne.balance; // using the function as property.
+  console.log(`My balance is: $${myBalance2}`);
+
+  // ==============================================================
+}
