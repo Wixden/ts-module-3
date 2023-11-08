d2ca59ee83656a7c2ba7d07726378bb67804cd59
Wixden <watchonlytutorial@gmail.com>
2023-11-08 07:46:07 +0600

    getter and setter

--- src/getter-and-setter.ts
+++ src/getter-and-setter.ts
@@ -15,10 +15,10 @@
       this._type = type;
     }
 
-    addBalance(amount: number) {
-      const newBalance = (this._balance += amount);
-      return `$${amount} added and new balance $${newBalance}`;
-    }
+    // addBalance(amount: number) {
+    //   const newBalance = (this._balance += amount);
+    //   return `$${amount} added and new balance $${newBalance}`;
+    // }
 
     // doing above function using getter and setter
     // getter
@@ -26,19 +26,27 @@
       return this._balance;
     }
 
-    getBalance() {
-      return this._balance;
+    // setter
+    set deposit(amount: number) {
+      this._balance = this.balance + amount;
     }
+
+    // getBalance() {
+    //   return this._balance;
+    // }
   }
 
   const personOne = new BankAccount(134567, "John Smith", 56000, "Savings");
   personOne.name = "Mark Benjamin";
 
-  const addMoney = personOne.addBalance(5000);
-  console.log(addMoney);
+  // const addMoney = personOne.addBalance(5000);
+  // console.log(addMoney);
 
-  const myBalance = personOne.getBalance();
+  // const myBalance = personOne.getBalance();
   // console.log(`My balance: $${myBalance}`);
+
+  const addMoney = (personOne.deposit = 78000);
+  console.log(addMoney);
 
   const myBalance2 = personOne.balance; // using the function as property.
   console.log(`My balance is: $${myBalance2}`);
